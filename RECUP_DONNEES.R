5c1c173c33a928c158ce8eb70c5056e52c43ece4
dtacon <dominique.tacon@insee.fr>
2023-06-27 08:37:26 +0000

    premier programme

---
+++ RECUP_DONNEES.R
@@ -0,0 +1,14 @@
+requirements <- readLines("requirements_R.txt")
+for (package in requirements) {
+  install.packages(package)
+}
+library(aws.s3)
+library(dplyr)
+library(readr)
+bucket <- "projet-funathon"
+path_data <- "2023/sujet3/diffusion"
+
+description_indiv <- s3read_using(read_delim, object = paste(path_data, "description-indiv.csv", sep="/"), bucket = bucket, opts = list('region'=''), show_col_types = FALSE)
+habitudes_indiv <- s3read_using(read_delim, object = paste(path_data, "habitudes-indiv.csv", sep="/"), bucket = bucket, opts = list('region'=''), show_col_types = FALSE)
+actphys_sedent <- s3read_using(read_delim, object = paste(path_data, "actphys-sedent.csv", sep="/"), bucket = bucket, opts = list('region'=''), show_col_types = FALSE)
+fpq <- s3read_using(read_delim, object = paste(path_data, "fpq.csv", sep="/"), bucket = bucket, opts = list('region'=''), show_col_types = FALSE)
